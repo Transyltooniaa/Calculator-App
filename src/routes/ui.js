e51873979ac894b38f01c6c57d76bb6556d4bd50
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2025-11-03 11:18:27 +0530

    Clearning up

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -3,13 +3,7 @@ const router = Router();
 
 router.get('/', (_req, res) => {
   res.render('index', {
-    title: 'Calculator',
-    ops: [
-      { value: 'sqrt', label: 'Square Root' },
-      { value: 'fact', label: 'Factorial' },
-      { value: 'ln', label: 'Logarithm' },
-      { value: 'pow', label: 'Power' }
-    ]
+    title: 'Calculator'
   });
 });
 
